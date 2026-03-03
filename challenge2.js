c9f850b1f31b53c2ff2cd70ae0a10ea09c0f5c87
Taiyo Yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2026-03-03 18:11:17 +0700

    fix question

--- challenge2.js
+++ challenge2.js
@@ -1,14 +1,8 @@
-// challenge2.js - Print O X O X O ... pattern
+// challenge2.js - Print OXOXO ... pattern by given n number of characters.
 // then export the function for testing by using named export
 
 function challenge2(n) {
   let result = '';
-  for (let i = 1; i <= n; i++) {
-    if (i % 2 === 1) {
-      result += 'O';
-    } else {
-      result += 'X';
-    }
-  }
+  
   return result;
 }
