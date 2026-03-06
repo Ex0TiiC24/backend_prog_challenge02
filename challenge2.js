341500d230b75006964c11516e10c985bd5c570a
Taiyo Yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2026-03-06 22:09:50 +0700

    fix typo

--- challenge2.js
+++ challenge2.js
@@ -1,9 +1,11 @@
 // challenge2.js - Print OXOXO ... pattern by given n number of characters.
 // then export the function for testing by using named export
-// then go to test2.js and import the function challenge2 function
+// then go to test2.js then import the challenge2 function
 
 function challenge2(n) {
   let result = '';
 
   return result;
 }
+
+console.log(challenge2(5)); // OXOXO
