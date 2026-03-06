e57c966b03c1052844dfb1dddce6524eb88bd8bc
Taiyo Yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2026-03-06 22:06:34 +0700

    add desc

--- challenge2.js
+++ challenge2.js
@@ -1,8 +1,9 @@
 // challenge2.js - Print OXOXO ... pattern by given n number of characters.
 // then export the function for testing by using named export
+// then go to test2.js and import the function challenge2 function
 
 function challenge2(n) {
   let result = '';
-  
+
   return result;
 }
